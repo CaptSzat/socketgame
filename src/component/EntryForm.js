db4127d36f67bbb4233c7eb08a2c0f31dbab1aea
CaptSzat <49853752+CaptSzat@users.noreply.github.com>
2021-08-19 17:55:21 +1000

    d

--- src/component/EntryForm.js
+++ src/component/EntryForm.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import App, {socket} from '../App';
+import React, { useState } from 'react';
+import {socket} from '../App';
 import '../App.css';
 
 
